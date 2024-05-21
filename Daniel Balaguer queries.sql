899ef9557e40b54930de411a62f7385df44702fd
dBalag <167875032+dBalag@users.noreply.github.com>
2024-05-21 08:00:24 +0200

    Update Daniel Balaguer queries.sql

--- Daniel Balaguer queries.sql
+++ Daniel Balaguer queries.sql
@@ -1,11 +1,18 @@
 /* 
 Query 1: 7 STEP FUNNEL QUERY 
 
-This query summarizes count of users & count of rides x each of the 7 step funnel, x each of the 3 platforms, x 5 age_ranges 
-& x each download_date.
-Its purpose is to get insights from the step funnel at a deeper levels (platforms, age_ranges, download_date).
-I used this query in Tableau to make viz and being able to answer business questions 1, 2, 3 & 5.
+This query summarizes count of users & count of rides x each of the 7 step funnel (the stages that the user does: Download,
+Sign-up, Ride Request, Ride Accept, Ride Complete, Payment & Review), x each of the 3 platforms, x 5 age_ranges & x each 
+download_date.
+Its purpose is to get insights from the step funnel at a deeper level (platforms, age_ranges, download_date).
+I used this dataset in Tableau to make viz and being able to answer these business questions:
 
+1. What steps of the funnel should we research and improve? Are there any specific drop-off points preventing users from 
+completing their first ride?
+2. Metrocar currently supports 3 different platforms: ios, android, and web. To recommend where to focus our marketing budget 
+for the upcoming year, what insights can we make based on the platform?
+3. What age groups perform best at each stage of our funnel? Which age group(s) likely contain our target customers?
+4. What part of our funnel has the lowest conversion rate? What can we do to improve this part of the funnel?
 */
 
 WITH 
@@ -16,7 +23,7 @@ downloaded AS   (SELECT count(ad.app_download_key) as downloaded, 0 as rides, ad
       			    left JOIN signups as s ON s.session_id=ad.app_download_key
       			    GROUP BY platform, age, download_time),
        
-signed AS 		(SELECT count(distinct s.user_id) as signed,  0 as rides,  ad.platform as platform, 
+signed AS 	(SELECT count(distinct s.user_id) as signed,  0 as rides,  ad.platform as platform, 
                 coalesce (s.age_range, 'Unknown') as age,  TO_CHAR(download_ts, 'MM/DD/YYYY') AS download_time
       				from signups s
       				left JOIN app_downloads as ad ON s.session_id = ad.app_download_key
@@ -27,7 +34,7 @@ requested AS 	(SELECT count(distinct rr.user_id) as requested, count( rr.ride_id
       				from ride_requests as rr
       				left JOIN signups as s ON s.user_id=rr.user_id
       				left JOIN app_downloads as ad ON ad.app_download_key=s.session_id
-            	    WHERE rr.request_ts is not null
+            	    		WHERE rr.request_ts is not null
       				GROUP BY platform, age, download_time),
                 
 accepted AS 	(SELECT count(distinct rr.user_id) as accepted, count( rr.ride_id) as rides_taken, ad.platform as platform, 
@@ -46,7 +53,7 @@ completed AS 	(SELECT count(distinct rr.user_id) as completed, count( rr.ride_id
       				WHERE rr.dropoff_ts is not null
       				GROUP BY platform, age, download_time),
                 
-paid AS 		(SELECT count(distinct rr.user_id) as paid, count( tr.ride_id) as rides_payed, ad.platform as platform, 
+paid AS 	(SELECT count(distinct rr.user_id) as paid, count( tr.ride_id) as rides_payed, ad.platform as platform, 
                 coalesce (s.age_range, 'Unknown') as age, TO_CHAR(download_ts, 'MM/DD/YYYY') AS download_time		
      	 			from transactions as tr
       				left JOIN ride_requests  as rr ON rr.ride_id=tr.ride_id
@@ -61,70 +68,70 @@ reviewed as 	(SELECT count (distinct r.user_id) as reviewed, count( r.ride_id) a
             	    left JOIN ride_requests  as rr ON rr.ride_id=r.ride_id
             	    left JOIN signups as s ON s.user_id=rr.user_id
             	    left JOIN app_downloads as ad ON ad.app_download_key=s.session_id
-           		    GROUP BY platform, age, download_time)
+           		GROUP BY platform, age, download_time)
     
  SELECT
-        1 AS funnel_step, 
-			'download' AS funnel_name, 
-			platform AS platform, 
-			age as age_range,
-  			download_time as download_date,
- 			downloaded AS number_of_users,
-  			rides AS number_of_rides
+        	1 AS funnel_step, 
+		'download' AS funnel_name, 
+		platform AS platform, 
+		age as age_range,
+  		download_time as download_date,
+ 		downloaded AS number_of_users,
+  		rides AS number_of_rides
     		FROM downloaded
     
 UNION ALL SELECT
-        2 AS funnel_step,
-            'signup' AS funnel_name,
-            platform AS platform, age as age_range, 
-            download_time as download_date,
-            signed AS number_of_users,
-            rides AS number_of_rides
+        	2 AS funnel_step,
+            	'signup' AS funnel_name,
+            	platform AS platform, age as age_range, 
+            	download_time as download_date,
+            	signed AS number_of_users,
+            	rides AS number_of_rides
     		FROM signed
    
 UNION ALL SELECT  
 		3 AS funnel_step,
-            'ride_requested' AS funnel_name,
-            platform AS platform, age as age_range,
-            download_time as download_date,
-            requested AS number_of_users,
-            rides AS number_of_rides
+            	'ride_requested' AS funnel_name,
+            	platform AS platform, age as age_range,
+            	download_time as download_date,
+            	requested AS number_of_users,
+            	rides AS number_of_rides
      		FROM requested
  
 UNION ALL SELECT  
 		4 AS funnel_step,
-            'ride_accepted' AS funnel_name,
-            platform AS platform, age as age_range, 
-            download_time as download_date,
-            accepted AS number_of_users,
-            rides_taken AS number_of_rides
+            	'ride_accepted' AS funnel_name,
+        	platform AS platform, age as age_range, 
+            	download_time as download_date,
+            	accepted AS number_of_users,
+            	rides_taken AS number_of_rides
         	FROM accepted
   
 UNION ALL SELECT 
 		5 AS funnel_step,
-            'ride_completed' AS funnel_name,
-            platform AS platform, age as age_range, 
-            download_time as download_date,
-            completed AS number_of_users,
-            rides_comp AS number_of_rides
-            FROM completed
+            	'ride_completed' AS funnel_name,
+            	platform AS platform, age as age_range, 
+            	download_time as download_date,
+            	completed AS number_of_users,
+            	rides_comp AS number_of_rides
+            	FROM completed
 
 UNION ALL SELECT 
 		6 AS funnel_step,
-            'payment' AS funnel_name,
-            platform AS platform, age as age_range, 
-            download_time as download_date,
-            paid AS number_of_users,
-            rides_payed AS number_of_rides
-            FROM paid
+            	'payment' AS funnel_name,
+            	platform AS platform, age as age_range, 
+           	download_time as download_date,
+            	paid AS number_of_users,
+            	rides_payed AS number_of_rides
+            	FROM paid
  
 UNION ALL SELECT 
 		7 AS funnel_step,
-            'review' AS funnel_name,
-            platform AS platform, age as age_range, 
-            download_time as download_date,
-            reviewed AS number_of_users,
-            rides_reviewed AS number_of_rides
+            	'review' AS funnel_name,
+            	platform AS platform, age as age_range, 
+            	download_time as download_date,
+            	reviewed AS number_of_users,
+           	 rides_reviewed AS number_of_rides
     		FROM reviewed
 
 ORDER BY funnel_step, platform, age_range, download_date;
@@ -135,8 +142,10 @@ ORDER BY funnel_step, platform, age_range, download_date;
 Query 2: RIDES REQUESTED X HOUR
 
 This query summarizes all the rides has been requested in each hour of the day. 
-Its purpose is to summarize x each hour how many rides has been requested, with that then being able to use it in Tableau and viz it to 
-answer the business question number 4 about Price Surging Strategy.
+Its purpose is to summarize x each hour how many rides has been requested, with that then being able to use it in Tableau and 
+viz it to answer the business question: 
+1. Surge pricing is the practice of increasing the price of goods or services when there is the greatest demand for them. 
+If we want to adopt a price-surging strategy, what does the distribution of ride requests look like throughout the day?.
 */
 
 WITH rides_requested AS     ( SELECT EXTRACT(HOUR FROM rr.request_ts) as request_hour, ad.platform as platform, s.age_range as age_range,
